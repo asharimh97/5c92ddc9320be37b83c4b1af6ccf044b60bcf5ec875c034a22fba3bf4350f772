aed1864f53bf295077be601eda13518ab020a2e8
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-10-09 06:48:59 +0700

    Implement actions on page components

--- omdb-project/src/pages/all-movies.js
+++ omdb-project/src/pages/all-movies.js
@@ -1,27 +1,116 @@
-import React, { useEffect } from "react";
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { getAllMovies } from "../stores/actions";
 
-let AllMovies = ({ getAllMovies }) => {
-  useEffect(() => {
-    const params = {
-      s: "Batman"
+import Layout from "../components/Layout";
+import BannerCover from "../components/MovieList/BannerCover";
+
+import { getAllMovies, getNextPage } from "../stores/actions";
+import ListMovies from "../components/MovieList/ListMovies";
+
+class AllMovies extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    const defaultKeyword = "Justice league";
+
+    this.state = {
+      page: 1,
+      keyword: defaultKeyword,
+      loading: false,
+      completed: false
     };
-    getAllMovies(params);
-  });
+  }
 
-  return (
-    <div>
-      <p>Ini halaman all movies</p>
-    </div>
-  );
-};
+  componentDidMount() {
+    this.getMovies();
+  }
 
-const mapStateToProps = state => ({});
+  getMovies = async () => {
+    const { keyword, page } = this.state;
+    const { getAllMovies } = this.props;
+
+    const params = {
+      s: keyword,
+      page
+    };
+
+    await getAllMovies(params);
+  };
+
+  handleSearchMovie = async val => {
+    await this.setState({ keyword: val, page: 1 });
+    this.getMovies();
+  };
+
+  handleNextPage = async () => {
+    const { page, keyword, loading, completed } = this.state;
+    const { getNextMovies, movies, total } = this.props;
+
+    const params = {
+      s: keyword,
+      page: page + 1
+    };
+
+    if (!loading && !completed) {
+      this.setState({ page: page + 1, loading: true });
+      const getData = await getNextMovies(params);
+
+      if (getData) {
+        this.setState({ loading: false });
+      } else {
+        // eslint-disable-next-line eqeqeq
+        if (movies.length == total) {
+          this.setState({ loading: false, completed: true });
+        }
+      }
+    }
+  };
+
+  render() {
+    const { keyword, loading, completed } = this.state;
+    return (
+      <Layout>
+        <BannerCover
+          data-testid="banner-cover"
+          onSearch={this.handleSearchMovie}
+          keyword={keyword}
+        />
+        <ListMovies
+          data-testid="list-movies"
+          keyword={keyword}
+          onLoadMore={this.handleNextPage}
+        />
+        {loading && (
+          <p
+            data-testid="loading-next-page"
+            className="text-center text-gray-500 text-sm"
+          >
+            Loading more movies...
+          </p>
+        )}
+        {completed && (
+          <p
+            data-testid="complete-load-movies"
+            className="text-center text-gray-500 text-sm"
+          >
+            All movies has been loaded
+          </p>
+        )}
+      </Layout>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  movies: state.movie.movies,
+  total: state.movie.total
+});
 
 const mapDispatchToProps = dispatch => ({
-  getAllMovies: bindActionCreators(getAllMovies, dispatch)
+  getAllMovies: bindActionCreators(getAllMovies, dispatch),
+  getNextMovies: bindActionCreators(getNextPage, dispatch)
 });
 
 AllMovies = connect(mapStateToProps, mapDispatchToProps)(AllMovies);
--- omdb-project/src/pages/movie-detail.js
+++ omdb-project/src/pages/movie-detail.js
@@ -1,13 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import { useParams } from "react-router-dom";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import Layout from "../components/Layout";
+import { getDetailMovie } from "../stores/actions";
+import MovieHeader from "../components/DetailMovie/MovieHeader";
+import RelatedMovie from "../components/DetailMovie/RelatedMovie";
 
-const MovieDetail = () => {
+let MovieDetail = ({ movieDetail, getDetailMovie, ...props }) => {
+  const [loading, setLoading] = useState(true);
+  const [failedLoad, setFailedLoad] = useState(false);
   const { movieId } = useParams();
+
+  const getDetail = async () => {
+    const params = {
+      i: movieId
+    };
+    const detail = await getDetailMovie(params);
+
+    if (detail) {
+      setLoading(false);
+    } else {
+      setLoading(false);
+      setFailedLoad(true);
+    }
+  };
+
+  useEffect(() => {
+    getDetail();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div>
-      <p>Ini halaman detail movies, id: {movieId}</p>
-    </div>
+    <Layout data-testid="page-detail-movie">
+      {loading ? (
+        "Loading..."
+      ) : failedLoad ? (
+        <p>Incorrect IMDB ID</p>
+      ) : (
+        <>
+          <MovieHeader
+            data-testid="movie-header-detail"
+            movie={movieDetail[movieId]}
+          />
+          <RelatedMovie
+            data-testid="related-movie"
+            movies={new Array(6).fill(movieDetail[movieId])}
+          />
+        </>
+      )}
+    </Layout>
   );
 };
+
+MovieDetail.propTypes = {
+  movieDetail: PropTypes.object,
+  getDetailMovie: PropTypes.func
+};
+
+const mapStateToProps = state => ({
+  movieDetail: state.movie.detailMovie
+});
+
+const mapDispatchToProps = dispatch => ({
+  getDetailMovie: bindActionCreators(getDetailMovie, dispatch)
+});
+
+MovieDetail = connect(mapStateToProps, mapDispatchToProps)(MovieDetail);
 
 export default MovieDetail;
